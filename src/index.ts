5ef7d75666f4a05340e26486bee6f3cd6c77fef6
arredondode <104038069+arredondode@users.noreply.github.com>
2022-05-12 17:28:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/*cocinarUnaTorta*/
+let ingrediente1: string = "400 gr de dulce de leche";
+let ingrediente2: string = "300 gr de queso blanco";
+let ingrediente3: string = "250 gr de galletitas de chococlate";
+let ingrediente4: string = "cafe soluble";
+let ingrediente5: string = "licor de cafe";
+
+console.log(
+  "Paso 1: En un bowl mezclar " +
+    ingrediente1 +
+    " y " +
+    ingrediente2 +
+    " hasta lograr una mezcla homogénea."
+);
+console.log(
+  "Paso2: En una fuente, preparar " +
+    ingrediente4 +
+    " y agregar " +
+    ingrediente5
+);
+console.log(
+  "Ir mojando los " + ingrediente3 + " una a la vez y disponerlas en un molde."
+);
+console.log(
+  "Cubrir con una capa de la preparación dulce de leche y queso blanco."
+);
+console.log(
+  "Seguir haciendo capas de galletitas hasta que no quede lugar en el molde."
+);
+console.log("Llevar a la heladera durante por lo menos 4 horas.");
+console.log("A DISFRUTAR DE UNA RICA CHOCOTORTA");
